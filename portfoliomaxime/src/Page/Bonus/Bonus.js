8a3d595763bf16df59f1283b466dab1b0b2a6075
Maxime Baratange <mbaratange@gmail.com>
2024-03-21 21:18:00 +0100

    Bonus version desktop

--- portfoliomaxime/src/Page/Bonus/Bonus.js
+++ portfoliomaxime/src/Page/Bonus/Bonus.js
@@ -4,20 +4,24 @@ import useMediaQuery from "../../Utils/useMediaQuery";
 import DataBDIntro from "../../Data/DataBD/DataBDIntro/DataBDIntro.json"
 import DataBDPLT from "../../Data/DataBD/DataBDPLT/DataBDPLT.json"
 import DataBDALT from "../../Data/DataBD/DataBDALT/DataBDALT.json"
-import { MenuBDMobileFirstImage, MenuBDMobileImage,  MenuBDMobileText, MenuBDMobileTextText, MenuBDMobileTitleText, MenuBonusMobileComponentLeft, MenuBonusMobileVue } from './BonusMobileStyle';
+import DataBDMS0 from "../../Data/DataBD/DataBDMS0/DataBDMS0.json"
+import { MenuBDMobileFirstImage, MenuBDMobileImage,  MenuBDMobileLeftImage,  MenuBDMobileText, MenuBDMobileTextText, MenuBDMobileTitleText, MenuBonusMobileComponentLeft, MenuBonusMobileVue } from './BonusMobileStyle';
+import { MenuBDFirstImage, MenuBDImage, MenuBDLeftImage, MenuBDText, MenuBDTextText, MenuBDTitleText, MenuBonusComponentLeft, MenuBonusVue } from './BonusStyle';
 
 function Bonus({pageIsIntro}){
     const [BDActive, setBDActive] = useState(null);
-    const [currentPage, setCurrentPage] = useState(0);
+    const [currentPage, setCurrentPage] = useState(1);
     const isDesktop = useMediaQuery('(min-width: 640px)');
     const buttonWrite = '← retour';
     const buttonWrite02 = '← Bonus';
-    const buttonWrite03 = '← Lire';
+    const buttonWrite03 = 'Lire →';
     const dataMap =(BD)=>{
         if(BD ==='DataBDALT')
         return DataBDALT;
         else if(BD ==='DataBDPLT')
-        return DataBDPLT
+        return DataBDPLT;
+        else if(BD ==='DataMS0')
+        return DataBDMS0;
     };
     return(
         <>
@@ -26,22 +30,83 @@ function Bonus({pageIsIntro}){
             </div>
             {BDActive?(
                 <div style={{width: '100px', padding:'10px'}}>
-                <Buttonbias write={buttonWrite02} newFunction={() => {setBDActive(null); setCurrentPage(0);}} isSkew={true} dimension={1}/>
+                <Buttonbias write={buttonWrite02} newFunction={() => {setBDActive(null); setCurrentPage(1);}} isSkew={true} dimension={1}/>
                 </div>
                 ):null
             }
         {isDesktop ? (
-            <p>Page Bonus</p>
+            BDActive ? (
+                <div style={{backgroundColor:"#001924", padding:"30px", height: "75%", border:"2px solid #B2E2F2", borderRadius:"5px", display: "flex", justifyContent:"center", gap:"20px", flexDirection: "column", alignItems: 'center'}}>
+                            <img src={process.env.PUBLIC_URL + dataMap(BDActive)[currentPage-1].page}
+                                alt={dataMap(BDActive)[currentPage-1].page} title={dataMap(BDActive)[currentPage-1].page}
+                                style={MenuBDLeftImage(true)}
+                                onClick={() => {
+                                    if (currentPage === dataMap(BDActive).length) { // si on est à la dernière page
+                                        setCurrentPage(1); // remettre currentPage à 0
+                                        setBDActive(null); // remettre BDActive à null
+                                    } else {
+                                        setCurrentPage(currentPage + 1); // sinon, passer à la page suivante
+                                    }
+                                }}
+                            />
+                            <div style={{display: "flex", justifyContent:"center", gap:"20px", flexDirection: "row", alignItems: 'center',}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50" fill='#B2E2F2'
+                            onClick={() => {
+                                if (currentPage === 1) { // si on est à la dernière page
+                                    setCurrentPage(1); // remettre currentPage à 0
+                                    setBDActive(null); // remettre BDActive à null
+                                } else {
+                                    setCurrentPage(currentPage - 1); // sinon, passer à la page suivante
+                                }
+                            }}>
+                                  <circle cx="12" cy="12" r="10" fill="none" stroke="#B2E2F2" stroke-width="1" />
+                                  <path d="M15 18l-6-6 6-6" stroke="#B2E2F2" stroke-width="1" stroke-linecap="round" />
+                                </svg>
+                                <p style={MenuBDTextText} >{currentPage} / {dataMap(BDActive).length}</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50" fill='#B2E2F2'
+                                onClick={() => {
+                                    if (currentPage === dataMap(BDActive).length) { // si on est à la dernière page
+                                        setCurrentPage(1); // remettre currentPage à 0
+                                        setBDActive(null); // remettre BDActive à null
+                                    } else {
+                                        setCurrentPage(currentPage + 1); // sinon, passer à la page suivante
+                                    }
+                                }}>
+                                  <circle cx="12" cy="12" r="10" fill="none" stroke="#B2E2F2" stroke-width="1" />
+                                  <path d="M9 6l6 6-6 6" stroke="#B2E2F2" stroke-width="1" stroke-linecap="round" />
+                                </svg>
+                            </div>
+            </div>
+        ) : (
+            <div style={MenuBonusVue}>
+                {DataBDIntro.map((data, index) => (
+                    <React.Fragment key={index}>
+                        <div style={MenuBonusComponentLeft}>
+                            <div style={MenuBDImage(true)}>
+                                <img src={process.env.PUBLIC_URL + data.Couv}
+                                    alt={data.Couv} title={data.Couv}
+                                    style={MenuBDFirstImage(true)}/>
+                            </div>
+                            <div style={MenuBDText(true)}>
+                                <p style={MenuBDTitleText}>{data.Title}</p>
+                                <p style={MenuBDTextText}>{data.Description}</p>
+                                <Buttonbias write={buttonWrite03} newFunction={() => setBDActive(data.Link)} isSkew={true} dimension={1}/>
+                            </div>
+                        </div>
+                    </React.Fragment>
+                ))}
+            </div>
+        )
         ) : (
         BDActive ? (
-            <div style={{backgroundColor:"#001924", padding:"30px", height: "100%", border:"2px solid #B2E2F2", borderRadius:"5px"}}>
-                <div>
-                        <img src={process.env.PUBLIC_URL + dataMap(BDActive)[currentPage].page}
-                            alt={dataMap(BDActive)[currentPage].page} title={dataMap(BDActive)[currentPage].page}
-                            style={MenuBDMobileFirstImage(true)}
+            <div style={{backgroundColor:"#001924", padding:"15px", height: "100%", border:"2px solid #B2E2F2", borderRadius:"5px", display: "flex", justifyContent:"center", gap:"20px", flexDirection: "column", alignItems: 'center'}}>
+                <div> {/* Mettre le nouveau code pour le scroll */}
+                        <img src={process.env.PUBLIC_URL + dataMap(BDActive)[currentPage-1].page}
+                            alt={dataMap(BDActive)[currentPage-1].page} title={dataMap(BDActive)[currentPage-1].page}
+                            style={MenuBDMobileLeftImage(true)}
                             onClick={() => {
-                                if (currentPage === dataMap(BDActive).length - 1) { // si on est à la dernière page
-                                    setCurrentPage(0); // remettre currentPage à 0
+                                if (currentPage === dataMap(BDActive).length) { // si on est à la dernière page
+                                    setCurrentPage(1); // remettre currentPage à 0
                                     setBDActive(null); // remettre BDActive à null
                                 } else {
                                     setCurrentPage(currentPage + 1); // sinon, passer à la page suivante
@@ -49,6 +114,34 @@ function Bonus({pageIsIntro}){
                             }}
                         />
                 </div>
+                        <div style={{display: "flex", justifyContent:"center", gap:"20px", flexDirection: "row", alignItems: 'center',}}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50" fill='#B2E2F2'
+                            onClick={() => {
+                                if (currentPage === 1) { // si on est à la dernière page
+                                    setCurrentPage(1); // remettre currentPage à 0
+                                    setBDActive(null); // remettre BDActive à null
+                                } else {
+                                    setCurrentPage(currentPage - 1); // sinon, passer à la page suivante
+                                }
+                            }}>
+                                  <circle cx="12" cy="12" r="10" fill="none" stroke="#B2E2F2" stroke-width="1" />
+                                  <path d="M15 18l-6-6 6-6" stroke="#B2E2F2" stroke-width="1" stroke-linecap="round" />
+                                </svg>
+                                <p style={MenuBDTextText} >{currentPage} / {dataMap(BDActive).length}</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50" fill='#B2E2F2'
+                                onClick={() => {
+                                    if (currentPage === dataMap(BDActive).length) { // si on est à la dernière page
+                                        setCurrentPage(1); // remettre currentPage à 0
+                                        setBDActive(null); // remettre BDActive à null
+                                    } else {
+                                        setCurrentPage(currentPage + 1); // sinon, passer à la page suivante
+                                    }
+                                }}>
+                                  <circle cx="12" cy="12" r="10" fill="none" stroke="#B2E2F2" stroke-width="1" />
+                                  <path d="M9 6l6 6-6 6" stroke="#B2E2F2" stroke-width="1" stroke-linecap="round" />
+                                </svg>
+                            </div>
+                
         </div>
     ) : (
         <div style={MenuBonusMobileVue}>
--- portfoliomaxime/src/Page/Bonus/BonusStyle.js
+++ portfoliomaxime/src/Page/Bonus/BonusStyle.js
@@ -1,22 +1,24 @@
-export const MenuBonusMobileVue = {
+export const MenuBonusVue = {
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'flex-start',
+    alignItems: 'flex-start',
+    margin: '40px',
+    gap:'20px'
+}
+export const MenuBonusComponentLeft = {
     display:'flex',
     flexDirection:'column',
     justifyContent:'center',
     alignItems: 'center',
-    margin: '30px 0 30px 0',
-    gap:'10px'
+    width: '30%',
 }
-export const MenuBonusMobileComponentLeft = {
-    display:'flex',
-    flexDirection:'column',
-    justifyContent:'center',
-    alignItems: 'center'
-}
+
 /* ------------------------------------------------------------------- */
-export const MenuBDMobileImage = (left) => ({
+export const MenuBDImage = (left) => ({
     border:"5px solid #B2E2F2",
     borderRadius: '2px 2px 2px 2px',
-    width: '76%',
+    width: '300px',
     display: 'grid',
     gridTemplateColumns: left? "2fr 1fr" : "1fr 2fr",
     gridTemplateRows:"1fr 1fr 1fr",
@@ -24,7 +26,7 @@ export const MenuBDMobileImage = (left) => ({
     gap:"5px",
     backgroundColor:"#10101C"
 });
-export const MenuBDMobileFirstImage = (left) => ({
+export const MenuBDFirstImage = (left) => ({
     objectFit: 'contain',
     width: "100%",
     height: "100%",
@@ -32,42 +34,41 @@ export const MenuBDMobileFirstImage = (left) => ({
     gridColumn: "1/3",
     gridRow:"1/4"
 });
-export const MenuBDMobileLeftImage = (left) => ({
+export const MenuBDLeftImage = (left) => ({
     objectFit: 'contain',
-    height: "65vh",
-    maxWidth: "100%"
+    height: "74vh",
 });
 
 /* ------------------------------------------------------------------- */
-export const MenuBDMobileText = (left) => ({
+export const MenuBDText = (left) => ({
     borderTop:"0px solid #B2E2F2",
     borderRight:"3px solid #B2E2F2",
     borderBottom:"3px solid #B2E2F2",
     borderLeft:"3px solid #B2E2F2", 
     borderRadius:'2px 2px 12px 12px', 
     margin: '0 10px 10px 0' , 
-    padding: '0 10px 5px 10px',
-    width: '67%',
+    padding: '0 40px 25px 40px',
+    width: '40%',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     backgroundColor:"rgba(17, 36, 52, 0.9)"
 });
-export const MenuBDMobileTitleText = {
+export const MenuBDTitleText = {
     fontFamily: "Electrolize,sans-serif,Arial,Verdana",
-    fontSize: "25px",
+    fontSize: "30px",
     fontWeight: "700",
     color: "#FFFFFF",
-    margin:'10px 0 0 0'
+    margin:'40px 0 0 0'
 }
-export const MenuBDMobileTextText = {
+export const MenuBDTextText = {
     fontFamily: "Electrolize,sans-serif,Arial,Verdana",
     fontSize: "15px",
     color: "#E0E0E0",
 }
-export const MenuBDMobileLinkText = {
+export const MenuBDLinkText = {
     fontFamily: "Electrolize,sans-serif,Arial,Verdana",
-    fontSize: "15px",
+    fontSize: "25px",
     color: "#B9DEDF",
     overflow:"hidden"
 }
